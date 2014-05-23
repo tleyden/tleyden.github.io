442bf889fe1a989a369c08c272ea0c1ebc25ca94
Traun Leyden <tleyden@couchbase.com>
2014-05-23 09:06:22 -0700

    oops, didn't mean to add that

--- source/_posts/go-mode-load.el
+++
@@ -1,13 +0,0 @@
-;;; go-mode-load.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-(provide 'go-mode-load)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; go-mode-load.el ends here
